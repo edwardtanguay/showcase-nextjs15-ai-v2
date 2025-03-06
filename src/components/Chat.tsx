a5ae2f9571e365e2a16660bab4b611d840c3e7ec
Edward Tanguay (windows) <edward@tanguay.info>
2025-03-06 09:36:48 +0100

    chat frontend in

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,16 +1,26 @@
 "use client";
+import { useChat } from "ai/react";
 
 export const Chat = () => {
-	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-		console.log("okok");
+	const { messages, input, handleInputChange, handleSubmit } = useChat({
+		api: "api/openai",
+	});
+
+	// const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+	// 	e.preventDefault();
+	// 	console.log("okok");
+	// };
+
+	const renderResponse = () => {
+		return <div>There are {messages.length} messages.</div>;
 	};
 
 	return (
 		<div>
 			<div>(the conversation)</div>
+			{renderResponse()}
 			<form onSubmit={(e) => handleSubmit(e)}>
-				<input name="input" type="text" />
+				<input name="input" type="text" value={input} onChange={handleInputChange} />
 				<button type="submit">Submit</button>
 			</form>
 		</div>
