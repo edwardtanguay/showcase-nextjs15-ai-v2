f459f201c162ec3d1576903deb09e5d3aeb8a7ee
Edward Tanguay (windows) <edward@tanguay.info>
2025-03-06 09:22:45 +0100

    click works

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,11 +1,18 @@
+"use client";
+
 export const Chat = () => {
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		console.log("okok");
+	};
+
 	return (
 		<div>
 			<div>(the conversation)</div>
-			<form onSubmit={handleSubmit}>
+			<form onSubmit={(e) => handleSubmit(e)}>
 				<input name="input" type="text" />
 				<button type="submit">Submit</button>
 			</form>
 		</div>
-	)
+	);
 };
